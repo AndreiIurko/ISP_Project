ef50d67d7f5195df26f465783d00667defc28709
AndreyYurko <andreyyurko01@gmail.com>
2024-01-17 18:41:31 +0300

    small fixes

--- cleanup.sh
+++ cleanup.sh
@@ -13,11 +13,10 @@ sudo ip link delete dev tap_company_1
 # sudo ip link delete dev tap_bgp_r_out
 # sudo ip link delete dev tap_bgp_peer
 # sudo ip link delete dev br_ix
-sudo ip link delete dev tap_host
 
 
-sysctl net.ipv4.ip_forward=0
-iptables -P FORWARD ACCEPT
-iptables -D FORWARD -i tap_host -o wlx502b73c902c9 -j ACCEPT
-iptables -D FORWARD -o tap_host -i wlx502b73c902c9 -j ACCEPT
+sudo sysctl net.ipv4.ip_forward=0
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -D FORWARD -i br_private -o wlx502b73c902c9 -j ACCEPT
+sudo iptables -D FORWARD -o br_private -i wlx502b73c902c9 -j ACCEPT
 
